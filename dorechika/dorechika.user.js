09d92cff45105731e83dc4bad9bb52e0afb12bda
umainjana <umainjana@gmail.com>
2013-09-11 05:52:58 +0000

    1.14
    git-svn-id: svn://www3029ue.sakura.ne.jp/ixa@152 c6511b31-667a-e011-824b-6cf049d29682

--- dorechika/dorechika.user.js
+++ dorechika/dorechika.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Dorechika
-// @version        1.12
+// @version        1.14
 // @namespace      https://sites.google.com/site/ixamukakin/
-// @description    どれ近 Ver. 1.13	20120903
+// @description    どれ近 Ver. 1.14	20130911
 // @include        http://*.sengokuixa.jp/user/?user_id=*
 // @match          http://*.sengokuixa.jp/user/?user_id=*
 // @include        http://*.sengokuixa.jp/war/fight_history.php*
@@ -12,7 +12,7 @@
 
 // 近い拠点はどれ？
 //
-// 2011/06/29 1.0  初版 
+// 2011/06/29 1.0  初版
 // 2011/07/14 1.01 IXAホスト名変更に対応
 // 2011/07/16 1.02 プロフィール画面のどれ近対応
 // 2011/07/17 1.03 敵襲状況のどれ近対応
@@ -20,6 +20,7 @@
 // 2011/02/24 1.11 ３章に対応（２章までにも対応）
 // 2011/03/01 1.12 新合戦の敵襲画面に対応
 // 2011/09/03 1.13 浅井家に対応
+// 2013/09/11 1.14 今川家に対応
 
 //
 // Mokoと同じjQuery初期化
@@ -78,10 +79,10 @@ function dore_main($) {
 
 	var mapcs = Array ('  ','織田家','足利家','武田家','上杉家','徳川家','毛利家',
 							 '浅井家','北条家','長宗我部家','島津家','大友家','最上家',
-							 '黒田家','石田家','伊達家','豊臣家');
+							 '黒田家','石田家','伊達家','豊臣家','今川家');
 	var mapx = Array ( -1, 1, 2, 3, 4, 5, 6,
 	                      7, 8, 9, 10, 11, 12,
-						  2, 12, 7, 11);
+						  2, 12, 7, 11, 8);
 						  
 
 	var Territ = function ( ttype, tname, pos, population, cond, map) {
